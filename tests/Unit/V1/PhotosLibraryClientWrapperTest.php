13511ac4d76666dbbcd547a45e0a2de68a1c67f6
Bebeto Alves <bebeto.contato@gmail.com>
2021-09-28 04:10:43 -0300

    Add support for PHP >= 7.3, PHPUnit 9, fix test errors (#31)
    * Drop support for PHP 5 and add support for PHP >= 7.3
    
    * Drop support for PHPUnit 4.8/5.0 and add support for PHPUnit 9
    
    * Convert old syntax to new syntax
    
    * Add return declaration for setUp method
    
    * Replace setExpectedException by expectException
    
    * Fix syntax error after resolving merge conflict
    
    Co-authored-by: Jan-Felix Schmakeit <jfschmakeit@users.noreply.github.com>

--- tests/Unit/V1/PhotosLibraryClientWrapperTest.php
+++ tests/Unit/V1/PhotosLibraryClientWrapperTest.php
@@ -41,7 +41,7 @@ class PhotosLibraryClientWrapperTest extends GeneratedTest
     private $mockCredentialsHandler;
     private $mockTransport;
 
-    protected function setUp()
+    protected function setUp(): void
     {
         $this->mockCredentialsHandler = $this->getMockBuilder(CredentialsWrapper::class)
             ->disableOriginalConstructor()
